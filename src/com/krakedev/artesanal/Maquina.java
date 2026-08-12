92370ad30581447acd8ecd95b2d04108d8a0964c
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-08-12 13:55:14 -0500

    Agregar metodo Recargar Cerveza junto a su test

--- src/com/krakedev/artesanal/Maquina.java
+++ src/com/krakedev/artesanal/Maquina.java
@@ -62,4 +62,13 @@ public class Maquina {
 	public void llenarMaquina() {
 	this.cantidadActual = this.capacidadMaxima - 100;
 	}
+	public boolean recargarCerveza(double cantidad) {
+		double limitePermitido = capacidadMaxima - 100;
+		if(cantidadActual + cantidad <= limitePermitido) {
+			cantidadActual = cantidadActual + cantidad;
+			return true;
+		}else {
+			return false;
+		}
 	}
+}
